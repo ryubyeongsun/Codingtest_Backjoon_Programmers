223c564b0ad186286616066344f99f28d26119fe
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-05-20 16:22:43 +0900

    [D2] Title: 조교의 성적 매기기, Time: 87 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+
+			float[] arr = new float[num];
+
+			for(int i = 0; i < num; i++) {
+				st = new StringTokenizer(br.readLine());
+				int med = Integer.parseInt(st.nextToken());
+				int gimal = Integer.parseInt(st.nextToken());
+				int sub = Integer.parseInt(st.nextToken());
+
+				arr[i] = (float)(med * 0.35 + gimal * 0.45 + sub * 0.20);
+			}
+
+			float krr = arr[k - 1]; 
+
+			float[] sort = arr.clone();
+			Arrays.sort(sort); 
+			int rank = 0;
+			for(int i = num - 1; i >= 0; i--) {
+				if(sort[i] == krr) {
+					rank = num - 1 - i;
+					break;
+				}
+			}
+
+			String[] grade = {"A+", "A0", "A-" , "B+" , "B0", "B-" , "C+", "C0", "C-", "D0"};
+			int per = num / 10;
+
+			String result = grade[rank / per];
+			System.out.println("#" + test_case + " " + result);
+		}
+	}
+}
