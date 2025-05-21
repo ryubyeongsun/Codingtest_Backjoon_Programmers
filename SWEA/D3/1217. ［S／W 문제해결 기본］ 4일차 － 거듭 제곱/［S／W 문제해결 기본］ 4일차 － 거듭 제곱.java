a3e9ff3d0b9074d65ecde0c53ffd9c89e1f9ae81
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-05-21 18:11:49 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 94 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+;
 
 
 class Solution
-{
+{	
+	static int result =1;
+	static int m;
+	static int n;
 	public static void main(String args[]) throws Exception
 	{
 		Scanner br = new Scanner(System.in);
@@ -10,16 +14,25 @@ class Solution
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
 			int num = br.nextInt();
-			
+		
 
-			int n = br.nextInt();
-			int m = br.nextInt();
-			int result = 1;
-			for(int i=0; i<m; i++){
-				result*=n;
-			}
-			System.out.println("#" + test_case+ " " + result);
+			n =br.nextInt();
+			m =br.nextInt();
+			
+			dfs(1);
+
+			System.out.println("#" + test_case + " " + result);
+			result=1;
 
 		}
+	}
+	static void dfs(int index){
+		if(m==index){
+			result*=n;
+			return;
+		}
+		result*=n;
+		dfs(index+1);
+
 	}
 }
