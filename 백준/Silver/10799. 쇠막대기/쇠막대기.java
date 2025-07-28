dc547ca6cfe87b43aab69ab32ba19584b82a4693
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-07-28 22:32:23 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16388 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,35 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine();
-        Stack sk = new Stack<>();
-        int num = 0;
-        sk.push('(');
-        for(int i=1; i<word.length(); i++){
-            
-            char current = word.charAt(i);
-            char prev = word.charAt(i-1);
-            if(prev == '('){
-                if(current == ')'){
-                    sk.pop();
-                    num+=sk.size();
-                }else{
-                    sk.push(current);
-                }
-            }else if(prev == ')'){
-                if(current == '('){
-                    sk.push(current);
-                }else{
-                    sk.pop();
-                    num+=1;
+
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int sum = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == '('){
+                stack.push(ch);
+            } else {
+                stack.pop();
+
+                if(str.charAt(i - 1) == '('){
+                    sum += stack.size();
+                } else {
+                    sum += 1;
                 }
             }
-
         }
-        System.out.println(num);
 
+        System.out.println(sum);
     }
 }
