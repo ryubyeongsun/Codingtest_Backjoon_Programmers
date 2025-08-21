f22de3b63a7e28c1e5708c4087f668a65833be39
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-08-21 14:47:26 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 137 ms, Memory: 27,624 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -1,25 +1,24 @@
+
 import java.util.*;
 import java.io.*;
 
 class Solution
 {
-    static int N;	
-    static int arr[][];
-    static int dx[] = {1,1,-1,-1};
-    static int dy[] = {1,-1,-1,1};
-    static boolean visit[][];
+    static int N;
+    static int arr[][]; 
+    static int dx[] = {1,1,-1,-1}; 
+    static int dy[] = {1,-1,-1,1}; 
     static boolean eat[];
     static int max;
-    public static void main(String args[]) throws IOException
+	public static void main(String args[]) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-		
+		int T = Integer.parseInt(br.readLine());
+	
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
             N = Integer.parseInt(br.readLine());
             arr = new int[N][N];
-            visit = new boolean[N][N];
             eat = new boolean[101];
 
             for(int i=0; i<N; i++){
@@ -29,44 +28,41 @@ class Solution
                 }
             }
             max = -1;
-
             for(int i=0; i<N; i++){
                 for(int j=0; j<N; j++){
-                    visit[i][j] = true;
-                    eat[arr[i][j]] =true;
-                    dfs(i, j, i, j, 1,0);
-                    visit[i][j] = false;
+                    eat[arr[i][j]] = true;
+                    dfs(i, j,i,j,1,0);
                     eat[arr[i][j]] = false;
                 }
+
+
             }
-            System.out.println("#" + test_case + " " +max);
+
+            System.out.println("#" + test_case + " " + max);
 
 		}
-
 	}
-    public static void dfs(int x, int y, int initX, int initY, int count,int dir){
-        for(int i=dir; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
+    public static void dfs(int x, int y,int initX, int initY ,int len, int dir){
 
-            if(nx == initX && ny == initY && count>=4){
-                max = Math.max(max, count);
+        for(int i=dir; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if( nx==initX && ny==initY && len >= 4){
+                max = Math.max(max, len);
                 return;
             }
-            
-            
-            if(nx>=0 && ny>=0 && nx<N && ny<N && !visit[nx][ny] && !eat[arr[nx][ny]] ){
-                visit[nx][ny] = true;
+
+            if(nx>=0 && ny>=0 && nx<N && ny<N && !eat[arr[nx][ny]]){
                 eat[arr[nx][ny]] = true;
-                dfs(nx, ny, initX, initY, count+1,i);
-                visit[nx][ny] = false;
-                eat[arr[nx][ny]]= false;
+                dfs(nx, ny,initX,initY,len+1,i);
+                eat[arr[nx][ny]] = false;
 
             }
+            
 
         }
 
-    }
 
-    
+    }
 }
