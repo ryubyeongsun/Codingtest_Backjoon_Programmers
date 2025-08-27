c155d6d8a47c6c1ea704c4eb494001d4fbf8ee87
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-08-27 17:04:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 96 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 class Solution
 {
+    static int N;
+    static int X;
     static int arr[][];
-    static int N,X;
     public static void main(String args[]) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,10 +14,10 @@ class Solution
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             N = Integer.parseInt(st.nextToken());
             X = Integer.parseInt(st.nextToken());
             arr = new int[N][N];
-            int total = 0;
 
             for(int i=0; i<N; i++){
                 st = new StringTokenizer(br.readLine());
@@ -25,54 +26,56 @@ class Solution
                 }
             }
 
+            int count = 0;
+
             for(int i=0; i<N; i++){
-                if(chk(arr[i])) total++;
+                if(chk(arr[i])){
+                    count++;
+                }
+
             }
-           
+
             for(int i=0; i<N; i++){
                 int col[] = new int[N];
                 for(int j=0; j<N; j++){
-                    col[j] = arr[j][i];
+                    col[j] = arr[j][i];  
                 }
-                if(chk(col)) total++;
-
+                if(chk(col)){
+                    count++;
+                }
             }
-            System.out.println("#" + test_case + " " + total);
 
+            System.out.println("#" + test_case + " " + count);
         }
-
 	}
     public static boolean chk(int n[]){
-        boolean use[] = new boolean[N];    
-
+        boolean use[] = new boolean[N];  
         for(int i=0; i<N-1; i++){
+
             if(n[i] == n[i+1]) continue;
 
-            if(Math.abs(n[i] - n[i+1]) > 1) return false;
+            if(Math.abs(n[i] - n[i+1]) > 1)  return false;
+
 
             if(n[i] - n[i+1] == 1){
                 for(int j=i+1; j<=i+X; j++){
-                    if(j>=N || n[j] != n[i+1] || use[j]) return false;
-                    use[j] = true;
-                }
-
-            }else if(n[i+1] - n[i] ==  1){
-                for(int j =i; j>i-X; j--){
-                    if(j<0 || n[j] != n[i] || use[j]) return false;
+                    if(j>=N || n[i+1] != n[j] || use[j]) return false;
                     use[j] = true;
                 }
 
             }
 
+            if(n[i+1] - n[i] == 1){
+                for(int j=i; j>i-X; j--){
+                    if(j<0 || n[i] != n[j] || use[j]) return false;
+                    use[j] = true;
+                }
+            }
+
         }
+
         return true;
-
     }
-  
-
 
 }
 
-    
-
-    
