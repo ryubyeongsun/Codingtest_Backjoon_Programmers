6981fcf31e98a3e632494fb0a8a2f33b8295e182
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-09-08 17:37:58 +0900

    [Gold IV] Title: 치즈, Time: 140 ms, Memory: 16400 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M, cheese, time;
+    static int[][] map;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        cheese = 0; time = 0;
+        
+        for(int i=0; i<N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<M; j++) {
+        		map[i][j] = Integer.parseInt(st.nextToken());
+        		if(map[i][j] == 1) {
+        			cheese++;
+        		}
+        	}
+        }
+        int lastCheese=0;
+        
+        while(cheese>0) {
+        	lastCheese = cheese;
+        	bfs();
+        	time++;
+        }
+       System.out.println(time);
+       System.out.print(lastCheese);
+        
+    }
+    public static void bfs() {
+    	Queue<int []> q = new LinkedList<>();
+    	boolean visit[][] = new boolean[N][M];
+    	visit[0][0] = true;
+    	q.offer(new int[] {0,0});
+    	
+    	while(!q.isEmpty()) {
+    		int cur[] = q.poll();
+    		for(int i=0; i<4; i++) {
+    			int nx = cur[0] + dx[i];
+    			int ny = cur[1] + dy[i];
+    			
+    			if(nx<0 || ny<0 || nx>=N || ny>=M || visit[nx][ny]) continue;
+    			visit[nx][ny] = true;
+    			
+    			if(map[nx][ny]==0) {
+    				q.offer(new int[] {nx,ny});
+    			}else {
+    				map[nx][ny] = 0;
+    				cheese--;
+    				
+    			}
+    			
+    		}
+    		
+    		
+    	}
+    	
+    }
+    
+}
