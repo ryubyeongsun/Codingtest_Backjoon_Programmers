dcea51ccb462c508e8214d6e32df5f5de5065e2a
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-09-12 15:52:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 363 ms, Memory: 34,672 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -3,56 +3,63 @@ import java.io.*;
 
 class Solution
 {
-	public static void main(String args[]) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            int max = 0;
+            int map[][] = new int[N][N];
+            ArrayList<int []> homePos = new ArrayList<>();
 
-            int arr[][] = new int[N][N];
-
+            // 배열 입력
             for(int i=0; i<N; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j=0; j<N; j++){
-                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    if(map[i][j]==1){
+                        homePos.add(new int[]{i,j});
+                    }
                 }
             }
 
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
+            int maxCount = 0;
 
-                    for(int K=1; K<=N+1; K++){
-                        int cost = K * K + (K - 1) * (K - 1);
+            int K = N%2==0 ? N+1 : N;
+            int cost = K*K + (K-1)*(K-1);
+
+            while(K > 0){
+                cost = K*K + (K-1)*(K-1);
+
+                for(int i=0; i<N; i++){
+                    for(int j=0; j<N; j++){
                         int count = 0;
-                        for(int r=0; r<N; r++){
-                            for(int c=0; c<N; c++){
-                                if(arr[r][c]==1 && Math.abs(i-r) + Math.abs(j-c) < K){
-                                    count++;
-                                }
-
+                        for(int d=0; d<homePos.size(); d++){
+                            if(mandist(homePos, d, i, j, K)){
+                                count++;
                             }
                         }
-                        int homecost = count*M;
-                        if(cost<=homecost){
-                            max= Math.max(max, count);
+
+                        if(count * M >= cost){
+                            maxCount = Math.max(maxCount, count);
                         }
-
                     }
-
-
                 }
+
+                K--; 
             }
 
-            System.out.println("#" + test_case + " " + max);
-
-		}
-
-	}
-    
+            System.out.println("#"+test_case+" "+maxCount);
+        }
+    }
+    public static boolean mandist(ArrayList<int[]> list, int idx, int x, int y, int k){
+        int []pos = list.get(idx);
+        int r = pos[0];
+        int c = pos[1];
+        return Math.abs(x-r) + Math.abs(y-c)<k;
+    }
 }
