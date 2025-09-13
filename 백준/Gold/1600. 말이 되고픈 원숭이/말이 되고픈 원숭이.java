efcdc14759518a97a5de144c463e7dd2123cafe0
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2025-09-14 01:38:59 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 484 ms, Memory: 89808 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,74 +1,74 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int K, W, H;
-    static int[][] arr;
-    static boolean[][][] visit;
-    static int[] dx = {-1, 1, 0, 0}; 
-    static int[] dy = {0, 0, -1, 1};
-    static int[] horseX = {-2, -2, -1, -1, 2, 2, 1, 1}; 
-    static int[] horseY = {1, -1, -2, 2, -1, 1, -2, 2};
-
+    static int k,r,c;
+    static int map[][];
+    static int dx[] = {-1,1,0,0};
+    static int dy[] = {0,0,-1,1};
+    static int horseX[] = {-2,-2,-1,-1,2,2,1,1};
+    static int horseY[] = {-1,1,-2,2,-1,1,-2,2};
+    static boolean visit[][][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        K = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
 
-        arr = new int[H][W];
-        for (int i = 0; i < H; i++) {
+        c = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        map = new int[r][c];
+        for(int i=0; i<r; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < W; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<c; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        visit = new boolean[r][c][k+1];
+        System.out.println(bfs(0,0,k));
 
-        System.out.println(bfs());
     }
-
-    public static int bfs() {
-        visit = new boolean[H][W][K+1];
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.offer(new int[]{0, 0, 0, 0});
-        visit[0][0][0] = true;
+    public static int bfs(int x, int y, int k){
+        Queue<int []> q = new LinkedList<>();
+        visit[x][y][k] = true;
+        q.offer(new int[]{x,y,k,0});
 
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1], k = cur[2], dist = cur[3];
+            int cur[] = q.poll();
+            int k1 = cur[2];
+            int len = cur[3];
 
-            if (x == H-1 && y == W-1) return dist;
+            if(cur[0] == r-1 && cur[1] == c-1) return len;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for(int i=0; i<4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
-                if (arr[nx][ny] == 1) continue;
-                if (visit[nx][ny][k]) continue;
+                if(nx<0 || ny<0 || nx>=r || ny>=c || visit[nx][ny][k1]) continue;
+                if(map[nx][ny] == 1 ) continue;
 
-                visit[nx][ny][k] = true;
-                q.offer(new int[]{nx, ny, k, dist+1});
+                visit[nx][ny][k1] = true;
+                q.offer(new int[]{nx,ny,k1,len+1});
+
             }
 
-            if (k < K) {
-                for (int i = 0; i < 8; i++) {
-                    int nx = x + horseX[i];
-                    int ny = y + horseY[i];
+            if(k1>0){
+                for(int i=0; i<8; i++){
+                    int nx = cur[0] + horseX[i];
+                    int ny = cur[1] + horseY[i];
 
-                    if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
-                    if (arr[nx][ny] == 1) continue;
-                    if (visit[nx][ny][k+1]) continue;
+                    if(nx<0 || ny<0 || nx>=r || ny>=c || visit[nx][ny][k1-1]) continue;
+                    if(map[nx][ny]==1) continue;
+                    visit[nx][ny][k1-1] = true;
+                    q.offer(new int[]{nx,ny,k1-1,len+1});
 
-                    visit[nx][ny][k+1] = true;
-                    q.offer(new int[]{nx, ny, k+1, dist+1});
                 }
-                
-            }
-        }
 
-        return -1; 
+            }
+        
+
+
+        }
+        return -1;
     }
+
 }
